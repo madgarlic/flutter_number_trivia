76f0e75a392babf4c66b122f936eebf8c5e26d5b
Mario Daglio <mdaglio@aeon-solutions.it>
2023-12-27 12:50:50 +0100

    Setup InputConverter to handle input in BloC

---
+++ test/core/util/input_converter_test.dart
@@ -0,0 +1,42 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_number_trivia/core/error/failures.dart';
+import 'package:flutter_number_trivia/core/util/input_converter.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  late InputConverter inputConverter;
+
+  setUp(() {
+    inputConverter = InputConverter();
+  });
+
+  group('stringToUnsignedInt', () {
+    test('sholud return an integer when the string represents an unsigned int',
+        () {
+      // arrange
+      const str = '42';
+      // act
+      final result = inputConverter.stringToUnsignedInteger(str);
+      // assert
+      expect(result, const Right(42));
+    });
+
+    test('sholud return failure when the string is not a number', () {
+      // arrange
+      const str = 'abc';
+      // act
+      final result = inputConverter.stringToUnsignedInteger(str);
+      // assert
+      expect(result, Left(InvalidInputFailure()));
+    });
+
+    test('sholud return failure when the string is a negative int', () {
+      // arrange
+      const str = '-42';
+      // act
+      final result = inputConverter.stringToUnsignedInteger(str);
+      // assert
+      expect(result, Left(InvalidInputFailure()));
+    });
+  });
+}
